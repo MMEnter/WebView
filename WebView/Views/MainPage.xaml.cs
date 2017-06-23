a9c9c938af3b1575cf075ba255b6db1c6578cf7a
Merlin Eck <eck_merlin@outlook.com>
2017-06-22 19:21:07 -0500

    Moved the UI into the TitleBar and added a TextBox to enter URL's.

--- WebView/Views/MainPage.xaml.cs
+++ WebView/Views/MainPage.xaml.cs
@@ -2,6 +2,8 @@ using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -13,13 +15,60 @@ namespace WebView.Views
         public MainPage()
         {
             InitializeComponent();
-            if (IsFullscreen())
+            if (ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay))
             {
-                CommandBar.Visibility = Visibility.Collapsed;
+                MiniMode.Visibility = Visibility.Visible;
             }
+
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            TitleBar.Height = coreTitleBar.Height;
+            Window.Current.SetTitleBar(MainTitleBar);
+
+            Window.Current.Activated += Current_Activated;
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Current_Activated(object sender, WindowActivatedEventArgs e)
+        {
+            if (e.WindowActivationState != CoreWindowActivationState.Deactivated)
+            {
+                BackButtonGrid.Visibility = Visibility.Visible;
+                MainTitleBar.Opacity = 1;
+
+                TitleBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BackButtonGrid.Visibility = Visibility.Collapsed;
+                MainTitleBar.Opacity = 0.5;
+
+                var view = ApplicationView.GetForCurrentView();
+                if(view.ViewMode.ToString() == "CompactOverlay")
+                {
+                    TitleBar.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar titleBar, object args)
+        {
+            var view = ApplicationView.GetForCurrentView();
+            if (view.ViewMode.ToString() != "CompactOverlay")
+            {
+                TitleBar.Visibility = titleBar.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            TitleBar.Height = sender.Height;
+            RightMask.Width = sender.SystemOverlayRightInset;
+        }
 
         private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
         {
@@ -36,11 +85,15 @@ namespace WebView.Views
 
         private async void MiniView_Click(object sender, RoutedEventArgs e)
         {
-            //bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay);
-            ViewModePreferences compactOptions = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
-            compactOptions.CustomSize = new Windows.Foundation.Size(320, 200);
-            //bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.Default, compactOptions);
-            bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, compactOptions);
+            var view = ApplicationView.GetForCurrentView();
+            if (view.ViewMode.ToString() != "CompactOverlay")
+            {
+                bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay);
+            }
+            else
+            {
+                bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.Default);
+            }
         }
 
         private void Focus_Click(object sender, RoutedEventArgs e)
@@ -51,7 +104,7 @@ namespace WebView.Views
                 view.ExitFullScreenMode();
                 ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.Auto;
                 // The SizeChanged event will be raised when the exit from full-screen mode is complete.
-                CommandBar.Visibility = Visibility.Visible;
+                //CommandBar.Visibility = Visibility.Visible;
             }
             else
             {
@@ -59,7 +112,7 @@ namespace WebView.Views
                 {
                     ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
                     // The SizeChanged event will be raised when the entry to full-screen mode is complete.
-                    CommandBar.Visibility = Visibility.Collapsed;
+                    //CommandBar.Visibility = Visibility.Collapsed;
                 }
             }
 
@@ -83,7 +136,7 @@ namespace WebView.Views
         {
             if (!IsFullscreen())
             {
-                CommandBar.Visibility = Visibility.Visible;
+                //CommandBar.Visibility = Visibility.Visible;
             }
         }
     }
